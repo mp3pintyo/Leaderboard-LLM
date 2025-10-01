67ef2f32a16f7ea93a272e81f52cdcc5c070ab76
mp3pintyo <mp3pintyo@gmail.com>
2025-10-01 23:00:39 +0200

    new 2. task

--- config.py
+++ config.py
@@ -368,6 +368,22 @@ MODELS: Dict[str, Dict[str, Any]] = {
         'image_input': False,
         'input_price': [{'threshold': 'all', 'price': '$0.60'}],
         'output_price': [{'threshold': 'all', 'price': '$2.10'}]
+    },
+    'llm-023': {
+        'name': 'GLM-4.6-355B-A32B Reasoning',
+        'api_id': 'glm-4-6-355b-a32b-reasoning',
+        'open_source': True,
+        'tokenizer': 'unknown',
+        'reasoning': True,
+        'languages': ['en'],
+        'tags': ['general'],
+        'provider': 'Z.ai',
+        'parameters': 355,  # Billion parameters
+        'context_window': 200,  # Thousand tokens (K)
+        'release_date': '2025-09-30',
+        'image_input': False,
+        'input_price': [{'threshold': 'all', 'price': '$0.60'}],
+        'output_price': [{'threshold': 'all', 'price': '$2.10'}]
     }
 }
 
